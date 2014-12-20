179420c1b1fd605fdcc8d91c64d4dacf327d3593
Luke Martin <lukemartinwebdev@gmail.com>
2014-12-20 16:41:51 -0600

    Add Audio Context

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!doctype html>
 <html>
 <head>
+	<title>Musyn</title>
+	<script type='text/javascript' src="js/musyn.js"></script>
 </head>
 <body>
 	<h1>Musyn</h1>
---
+++ js/musyn.js
@@ -0,0 +1,8 @@
+var context;
+if (typeof AudioContext !== "undefined") {
+	context = new AudioContext();
+} else if (typeof webkitAudioContext !== "undefined") {
+	context = new webkitAudioContext();
+} else {
+	throw new Error('AudioContext not supported. :(');
+}
