27f830ad9c4073c06a5702ebde7921c234923283
Luke Martin <lukemartinwebdev@gmail.com>
2014-12-20 16:50:41 -0600

    Add style

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
 	<title>Musyn</title>
 	<script type='text/javascript' src="js/musyn.js"></script>
+	<link href='http://fonts.googleapis.com/css?family=Muli:300,400,300italic' rel='stylesheet' type='text/css'>
+	<link href='css/style.css' rel='stylesheet' type='text/css'>
 </head>
 <body>
 	<h1>Musyn</h1>
