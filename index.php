f2f4590cd5586bcf6968dc3eb5c27ce1286b26bd
Luke Martin <lukemartinwebdev@gmail.com>
2014-12-24 20:06:10 -0600

    Rebuild to make Audio Context oscillator based

--- index.php
+++ index.php
@@ -2,13 +2,12 @@
 <html>
 <head>
 	<title>Musyn</title>
-	<script type='text/javascript' src="js/musyn.js"></script>
 	<link href='http://fonts.googleapis.com/css?family=Muli:300,400,300italic' rel='stylesheet' type='text/css'>
 	<link href='css/style.css' rel='stylesheet' type='text/css'>
 </head>
 <body>
 	<h1>Musyn</h1>
-    <span id="play">Play</span>
-    <span id="stop">Stop</span>
+
+	<script type='text/javascript' src="js/musyn.js"></script>
 </body>
 </html>
